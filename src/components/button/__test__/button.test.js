a13d4bf97dc802fca47b5760fc6703d0ba12ea71
zoranaism <lazareviczorana@gmail.com>
2020-08-21 12:29:16 +0200

    snapshot, rendering and other testing

---
+++ src/components/button/__test__/button.test.js
@@ -0,0 +1,36 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Button from "./../button";
+// import { isTSAnyKeyword } from "@babel/types";
+
+import { render, cleanup } from "@testing-library/react";
+import "@testing-library/jest-dom/extend-expect";
+
+import renderer from "react-test-renderer";
+
+afterEach(cleanup);
+
+it("renders without crashing", () => {
+  const div = document.createElement("div");
+  ReactDOM.render(<Button></Button>, div);
+});
+
+it("renders button correctly", () => {
+  const { getByTestId } = render(<Button label="click me please"></Button>);
+  expect(getByTestId("button")).toHaveTextContent("click me please");
+});
+
+it("renders button correctly", () => {
+  const { getByTestId } = render(<Button label="save"></Button>);
+  expect(getByTestId("button")).toHaveTextContent("save");
+});
+
+it("matches snapshot 1", () => {
+  const tree = renderer.create(<Button label="save"></Button>).toJSON();
+  expect(tree).toMatchSnapshot();
+});
+
+it("matches snapshot 2", () => {
+  const tree = renderer.create(<Button label="click me please"></Button>).toJSON();
+  expect(tree).toMatchSnapshot();
+});
